b24c217cb9def5472d8a7ef87b44b92650d83c5b
Daniel de Sousa Oliveira Melo Veras <daniel_sousa.unb@outlook.com>
2020-09-30 23:03:07 -0300

    :alien: Refactoring code
    Co-authored-by: Pedro Miranda <pmiranda7410@gmail.com>
    Co-authored-by: Luis Bruno <l.brunofidelis@gmail.com>

--- sya-web/src/pages/SignIn/index.tsx
+++ sya-web/src/pages/SignIn/index.tsx
@@ -2,7 +2,7 @@ import React, { useCallback } from 'react';
 import { Form } from '@unform/web';
 
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
-import { Container, Content, Background } from './styles';
+import { Container, Content, Background, AnimationContainer } from './styles';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
@@ -17,27 +17,29 @@ const SignIn: React.FC = () => {
   return (
     <Container>
       <Content>
-        <img src={logoImg} alt="Logo SYA" />
+        <AnimationContainer>
+          <img src={logoImg} alt="Logo SYA" />
 
-        <Form onSubmit={handleSubmit}>
-          <h1>Logar</h1>
+          <Form onSubmit={handleSubmit}>
+            <h1>Logar</h1>
 
-          <Input name="email" icon={FiMail} placeholder="E-mail" />
-          <Input
-            name="password"
-            icon={FiLock}
-            type="password"
-            placeholder="Senha"
-          />
-          <a href="forgot">Esqueci minha senha</a>
+            <Input name="email" icon={FiMail} placeholder="E-mail" />
+            <Input
+              name="password"
+              icon={FiLock}
+              type="password"
+              placeholder="Senha"
+            />
+            <a href="forgot">Esqueci minha senha</a>
 
-          <Button type="submit">Entrar</Button>
-        </Form>
+            <Button type="submit">Entrar</Button>
+          </Form>
 
-        <a href="login">
-          <FiLogIn />
-          Criar conta
-        </a>
+          <a href="login">
+            <FiLogIn />
+            Criar conta
+          </a>
+        </AnimationContainer>
       </Content>
       <Background />
     </Container>
