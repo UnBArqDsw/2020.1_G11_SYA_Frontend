f10747cddbada30e6fcf5384c5baf894d8cdc55c
Daniel de Sousa Oliveira Melo Veras <daniel_sousa.unb@outlook.com>
2020-10-07 19:46:56 -0300

    :alien: Put elements on screen

--- sya-web/src/pages/Profile/index.tsx
+++ sya-web/src/pages/Profile/index.tsx
@@ -1,7 +1,77 @@
 import React from 'react';
+import {
+  FiArrowLeft,
+  FiPower,
+  FiCamera,
+  FiMail,
+  FiUser,
+  FiLock,
+} from 'react-icons/fi';
+import { Form } from '@unform/web';
+import { Container, Content, Photo, AvatarInput, Break } from './styles';
 
-import { Container } from './styles';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 
-const Profile: React.FC = () => <Container />;
+const Profile: React.FC = () => {
+  return (
+    <Container>
+      <header>
+        <div>
+          <FiArrowLeft />
+          <FiPower />
+        </div>
+      </header>
+      <Photo>
+        <AvatarInput>
+          <img
+            src="https://avatars1.githubusercontent.com/u/48137972?s=460&u=7471585d946d915235ff9532f65a4082788abb06&v=4"
+            alt="Daniel"
+          />
+          <label htmlFor="avatar">
+            <FiCamera />
+            <input type="file" id="avatar" onChange={() => console.log('Oi')} />
+          </label>
+        </AvatarInput>
+      </Photo>
+      <Break>
+        <Content>
+          <Form onSubmit={() => console.log('Oi')}>
+            <h1>Meu Perfil</h1>
+
+            <Input name="name" icon={FiUser} placeholder="Nome" />
+
+            <Input name="email" icon={FiMail} placeholder="E-mail" />
+
+            <Input
+              name="changeEmail"
+              icon={FiMail}
+              placeholder="Trocar e-mail"
+            />
+
+            <Button type="submit">Confirmar mudanças</Button>
+          </Form>
+        </Content>
+        <Content>
+          <Form onSubmit={() => console.log('Oi')}>
+            <h1>Meu Perfil</h1>
+
+            <Input name="name" icon={FiUser} placeholder="Nome" />
+
+            <Input name="email" icon={FiMail} placeholder="E-mail" />
+
+            <Input
+              name="changeEmail"
+              icon={FiMail}
+              placeholder="Trocar e-mail"
+            />
+
+            <Button type="submit">Confirmar mudanças</Button>
+          </Form>
+        </Content>
+      </Break>
+    </Container>
+  );
+};
 
 export default Profile;
--- sya-web/src/pages/Profile/styles.ts
+++ sya-web/src/pages/Profile/styles.ts
@@ -1,3 +1,115 @@
 import styled from 'styled-components';
+import { shade } from 'polished';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  header {
+    height: 260px;
+    background: transparent;
+
+    display: flex;
+    align-items: center;
+
+    div {
+      display: flex;
+      justify-content: space-between;
+
+      width: 100%;
+      max-width: 1640px;
+      margin: 0 auto;
+
+      svg {
+        color: #fcfcfc;
+        width: 45px;
+        height: 45px;
+      }
+    }
+  }
+`;
+
+export const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  form {
+    display: flex;
+    flex-direction: column;
+    margin: 80px;
+    width: 340px;
+    text-align: center;
+
+    h1 {
+      margin-bottom: 36px;
+      font-weight: bold;
+      text-align: left;
+    }
+
+    a {
+      color: #f4ede8;
+      display: block;
+      margin-top: 24px;
+      text-decoration: none;
+      transition: color 0.2s;
+
+      &:hover {
+        color: ${shade(0.2, '#f4ede8')};
+      }
+    }
+  }
+`;
+
+export const Break = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Photo = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin: -180px auto 0;
+`;
+
+export const AvatarInput = styled.div`
+  position: relative;
+  align-self: center;
+
+  img {
+    width: 200px;
+    height: 200px;
+    border-radius: 50%;
+  }
+
+  label {
+    position: absolute;
+    width: 48px;
+    height: 48px;
+    border-radius: 50%;
+    background: #fcfcfc;
+    right: 0;
+    bottom: 0;
+    border: 0;
+    cursor: pointer;
+    transition: background-color 0.2s;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    input {
+      display: none;
+    }
+
+    svg {
+      width: 20px;
+      height: 20px;
+      color: #141212;
+    }
+
+    &:hover {
+      background: ${shade(0.2, '#fcfcfc')};
+    }
+  }
+`;
